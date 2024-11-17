16a98f037cfbc866ecdc5c03e52c6c241ed75f06
Dilshara <dilsharahasanka@gmail.com>
2024-11-17 14:47:41 +0530

    base layout

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+
+const products = [
+    {
+      categoryId: "1",
+      image: "/assets/products/airpods-max.png",
+      id: "1",
+      name: "AirPods Max",
+      price: "549.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "3",
+      image: "/assets/products/echo-dot.png",
+      id: "2",
+      name: "Echo Dot",
+      price: "99.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "2",
+      image: "/assets/products/pixel-buds.png",
+      id: "3",
+      name: "Galaxy Pixel Buds",
+      price: "99.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "1",
+      image: "/assets/products/quietcomfort.png",
+      id: "4",
+      name: "Bose QuiteComfort",
+      price: "249.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "3",
+      image: "/assets/products/soundlink.png",
+      id: "5",
+      name: "Bose SoundLink",
+      price: "119.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "5",
+      image: "/assets/products/apple-watch.png",
+      id: "6",
+      name: "Apple Watch 9",
+      price: "699.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "4",
+      image: "/assets/products/iphone-15.png",
+      id: "7",
+      name: "Apple Iphone 15",
+      price: "1299.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+    {
+      categoryId: "4",
+      image: "/assets/products/pixel-8.png",
+      id: "8",
+      name: "Galaxy Pixel 8",
+      price: "549.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?",
+    },
+  ];
+
+app.use(express.json()); // For parsing JSON requests
+
+const sendHello = (req, res) => res.send('Hello  !')
+
+app.get('/hello', sendHello);
+
+app.get('/products', (req, res) =>  res.status(200).json(products).send())
+
+
+
+app.listen(8000, () => console.log(`Server running on port ${8000}`));
