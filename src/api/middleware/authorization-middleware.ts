045e27c46ace61993a7fe613c597d4783837962f
ManupaDev <manupadevsuite@gmail.com>
2025-02-10 15:29:38 +0530

    feat: update role-based authorization with Clerk metadata

--- src/api/middleware/authorization-middleware.ts
+++ src/api/middleware/authorization-middleware.ts
@@ -3,7 +3,7 @@ import ForbiddenError from "../../domain/errors/forbidden-error";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
   const auth = req.auth;
-  if (auth.sessionClaims?.role !== "admin") {
+  if (auth.sessionClaims?.metadata.role !== "admin") {
     throw new ForbiddenError("Forbidden");
   }
 
