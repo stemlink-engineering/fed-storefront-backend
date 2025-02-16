d2a772d3011f4ca3bd73f7aa9a205244f2549578
ManupaDev <manupadevsuite@gmail.com>
2025-02-16 15:25:24 +0530

    Revert "feat: integrate Stripe payment flow with embedded checkout"
    This reverts commit 7a3651acdca43eb6e58df7474843e6b968275863.

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { createCheckoutSession, handleWebhook, retrieveSessionStatus } from "../application/payment";
+import { handleWebhook } from "../application/payment";
 
 export const paymentsRouter = express.Router();
 
 paymentsRouter.route("/webhook").post(handleWebhook);
-paymentsRouter.route("/create-checkout-session").post(createCheckoutSession);
-paymentsRouter.route("/session-status").get(retrieveSessionStatus);
+
--- src/infrastructure/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(
-  "sk_test_51Qg642JjbWEvglIUO1RBVu0BHwh3wOkJ5pglInZSHBq16uYu9xFNQbEijAZRjqKGQLKRe19i88yQXyONhy18LOTE00Z7ZV34Ay"
-);
-
-export default stripe;
