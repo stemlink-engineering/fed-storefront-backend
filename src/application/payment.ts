55a9c6fd6edfcde6c355e91817c460ebc508a962
ManupaDev <manupadevsuite@gmail.com>
2025-03-21 20:40:59 +0530

    fix: validate order status and update payment status upon successful checkout session fulfillment

--- src/application/payment.ts
+++ src/application/payment.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import util from "util";
 import Order from "../infrastructure/schemas/Order";
 import stripe from "../infrastructure/stripe";
-import util from "util";
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -21,15 +21,33 @@ async function fulfillCheckout(sessionId: string) {
   const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ["line_items"],
   });
+  console.log(
+    util.inspect(checkoutSession, false, null, true /* enable colors */)
+  );
 
-  console.log(util.inspect(checkoutSession, false, null, true /* enable colors */));
-  
+  const order = await Order.findById(checkoutSession.metadata?.orderId);
+  if (!order) {
+    throw new Error("Order not found");
+  }
+
+  if (order.paymentStatus !== "PENDING") {
+    throw new Error("Payment is not pending");
+  }
+
+  if (order.orderStatus !== "PENDING") {
+    throw new Error("Order is not pending");
+  }
+
   // Check the Checkout Session's payment_status property
   // to determine if fulfillment should be peformed
   if (checkoutSession.payment_status !== "unpaid") {
     // TODO: Perform fulfillment of the line items
     // TODO: Record/save fulfillment status for this
     // Checkout Session
+    await Order.findByIdAndUpdate(order._id, {
+      paymentStatus: "PAID",
+      orderStatus: "CONFIRMED",
+    });
   }
 }
 
@@ -61,7 +79,7 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
   const orderId = req.body.orderId;
   console.log("body", req.body);
   const order = await Order.findById(orderId);
- 
+
   if (!order) {
     throw new Error("Order not found");
   }
